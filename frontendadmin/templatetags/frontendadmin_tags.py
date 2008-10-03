b4d16c3710ffb2cb1de09b202d326863f792cff2
Martin Mahner <martin@mahner.org>
2008-10-03 17:27:05 +0200

    Changed the way, a template would provide model/appname to the templatetag.

--- frontendadmin/templatetags/frontendadmin_tags.py
+++ frontendadmin/templatetags/frontendadmin_tags.py
@@ -8,8 +8,10 @@ from frontendadmin.views import check_permission
 register = template.Library()
 
 @register.inclusion_tag('frontendadmin/link_add.html', takes_context=True)
-def frontendadmin_add(context, appmodelname, label=None):
-    app_label, model_name = appmodelname.split('.')
+def frontendadmin_add(context, queryset_object, label=None):
+
+    app_label = queryset_object.model._meta.app_label
+    model_name = queryset_object.model._meta.module_name
 
     template_context = {
         'add_link': reverse('frontendadmin_add', kwargs={
@@ -28,13 +30,16 @@ def frontendadmin_add(context, appmodelname, label=None):
     return template_context
 
 @register.inclusion_tag('frontendadmin/link_edit.html', takes_context=True)
-def frontendadmin_change(context, appmodelname, instance_id, label=None):
-    app_label, model_name = appmodelname.split('.')
+def frontendadmin_change(context, model_object, label=None):
+
+    app_label = model_object._meta.app_label
+    model_name = model_object._meta.module_name
+
     template_context = {
         'edit_link': reverse('frontendadmin_change', kwargs={
             'app_label': app_label,
             'model_name': model_name,
-            'instance_id': instance_id,
+            'instance_id': model_object.pk,
         }),
         'next_link': context['request'].META['PATH_INFO'],
         'label': label,
@@ -48,13 +53,16 @@ def frontendadmin_change(context, appmodelname, instance_id, label=None):
     return template_context
 
 @register.inclusion_tag('frontendadmin/link_delete.html', takes_context=True)
-def frontendadmin_delete(context, appmodelname, instance_id, label=None):
-    app_label, model_name = appmodelname.split('.')
+def frontendadmin_delete(context, model_object, label=None):
+
+    app_label = model_object._meta.app_label
+    model_name = model_object._meta.module_name
+
     template_context = {
         'delete_link': reverse('frontendadmin_delete', kwargs={
             'app_label': app_label,
             'model_name': model_name,
-            'instance_id': instance_id,
+            'instance_id': model_object.pk,
         }),
         'next_link': context['request'].META['PATH_INFO'],
         'label': label,
